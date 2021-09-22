f7cef25f557c874f4ae5454c61394e77cee52c19
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-09-22 15:04:34 +0700

    create UI for signup page

--- client/src/core/AppRouterConstant.ts
+++ client/src/core/AppRouterConstant.ts
@@ -1,16 +1,25 @@
+import SignUp from "../screens/AdminScreens/SignUp";
+
 enum ETYPE {
   ADMIN = "ADMIN",
-  USER = "USER"
+  USER = "USER",
 }
 
 interface IAppRouterConfig {
-  name: string,
-  path?: string,
-  exact?: boolean,
-  component?: React.FC<any> | React.FunctionComponent<any>,
-  type?: ETYPE
+  name: string;
+  path?: string;
+  exact?: boolean;
+  component?: React.FC<any> | React.FunctionComponent<any>;
+  type?: ETYPE;
 }
 
 // name => path => component => exact => type
-export const AppRouterConfig: IAppRouterConfig[] = [];
-
+export const AppRouterConfig: IAppRouterConfig[] = [
+  {
+    name: "Sign Up",
+    path: "/admin/sign-up",
+    component: SignUp,
+    exact: true,
+    type: ETYPE.ADMIN,
+  },
+];
