4ec4d863b5d23ca702707b24d7129ba3801159ca
DESKTOP-6J27DGC\phucp <phucphan98vn@gmail.com>
2021-09-22 17:46:03 +0700

    [issues 02] update format

--- client/src/core/AppRouterConstant.ts
+++ client/src/core/AppRouterConstant.ts
@@ -1,20 +1,31 @@
-import Homepage from '../screens/UserScreens/Homepage'
-import Login from '../screens/UserScreens/Login'
+import Homepage from "../screens/UserScreens/Homepage";
+import Login from "../screens/UserScreens/Login";
 enum ETYPE {
   ADMIN = "ADMIN",
-  USER = "USER"
+  USER = "USER",
 }
 
 interface IAppRouterConfig {
-  name: string,
-  path?: string,
-  exact?: boolean,
-  component?: React.FC<any> | React.FunctionComponent<any>,
-  type?: ETYPE
+  name: string;
+  path?: string;
+  exact?: boolean;
+  component?: React.FC<any> | React.FunctionComponent<any>;
+  type?: ETYPE;
 }
 // name => path => component => exact => type
 export const AppRouterConfig: IAppRouterConfig[] = [
-  {name:"home",path:"/home",component:Homepage,exact:true,type:ETYPE.USER},
-  {name:"login",path:"/login",component:Login,exact:false,type:ETYPE.USER}
+  {
+    name: "home",
+    path: "/home",
+    component: Homepage,
+    exact: true,
+    type: ETYPE.USER,
+  },
+  {
+    name: "login",
+    path: "/login",
+    component: Login,
+    exact: false,
+    type: ETYPE.USER,
+  },
 ];
-
