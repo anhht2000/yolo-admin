eb584d2f07bc54865287947fc4ea86baaf299a39
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-09-29 14:58:38 +0700

    fix conflict code

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -6,21 +6,21 @@ class ProductController {
   //get
   async getProduct(request: Request, response: Response, next: NextFunction) {
     try {
-      const { _page, _limit } = request.query;
-      const page = Number(_page) || 1;
-      const limit = Number(_limit) || 10;
+      const { page, _limit } = request.query;
+      const _page = Number(page) || 1;
+      const limit = Number(_limit) || 5;
       const count = await getRepository(Product)
         .createQueryBuilder("product")
         .getCount();
       const _total = Math.ceil(count / limit);
       const productList = await getRepository(Product)
         .createQueryBuilder("product")
-        .skip((page - 1) * limit)
+        .skip((_page - 1) * limit)
         .take(limit)
         .getMany();
       return response.status(200).json({
         data: productList,
-        page: _total,
+        _: _total,
       });
     } catch (error) {
       response.status(500).json({ err: error });
