40dea12270c99c4a0b138e72f0c40b868f18222e
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-09-29 18:21:47 +0700

    fix conflict

--- server/src/router/product.router.ts
+++ server/src/router/product.router.ts
@@ -7,34 +7,12 @@ const router = Router();
 router.get("/get", productController.getProduct);
 router.post(
   "/add",
-  [
-    body("name")
-      .isString()
-      .withMessage("Input phai la ky tu")
-      .isLength({ min: 3 })
-      .withMessage("Phai nhap it nhat 3 ky tu"),
-    body("price")
-      .isString()
-      .withMessage("Price phai la ky tu")
-      .isLength({ min: 3 })
-      .withMessage("Phai nhap it nhat 3 ky tu"),
-  ],
+  [body("name").isString().withMessage("Input phai la ky tu")],
   productController.addProduct
 );
 router.put(
   "/edit/:id",
-  [
-    body("name")
-      .isString()
-      .withMessage("Input phai la ky tu")
-      .isLength({ min: 3 })
-      .withMessage("Phai nhap it nhat 3 ky tu"),
-    body("price")
-      .isString()
-      .withMessage("Price phai la ky tu")
-      .isLength({ min: 3 })
-      .withMessage("Phai nhap it nhat 3 ky tu"),
-  ],
+  [body("name").isString().withMessage("Input phai la ky tu")],
   productController.updateProduct
 );
 router.delete("/delete/:id", productController.deleteProduct);
