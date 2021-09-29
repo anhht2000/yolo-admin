74eb02fbd23228a93c76c2f8d2900814c6738e3a
Đức Nguyễn <nguyenhuuduc110800@gmail.com>
2021-09-29 18:20:39 +0700

    fix comment

--- server/src/router/option.router.ts
+++ server/src/router/option.router.ts
@@ -1,20 +1,17 @@
 import { Router } from "express";
 import optionController from "../controllers/option.controller";
 import { body } from 'express-validator';
-import { ValidateData } from "../config/commonMiddleware";
+import { ValidateData } from "../middleware/commonMiddleware";
+import { createUpdateRuleOption } from "../rules/optionRouter.rule";
 const router = Router();
 
 router.get('/', optionController.getAllOption)
 
-router.post('/',[
-  body('name').isString().withMessage('Input nhập vào phải là ký tự'),
-], ValidateData ,optionController.createOption)
+router.post('/', createUpdateRuleOption, ValidateData, optionController.createOption)
 
-router.get('/:id',optionController.getOneOption)
+router.get('/:id', optionController.getOneOption)
 
-router.put('/:id', [
-  body('name').isString().withMessage('Input nhập vào phải là ký tự'),
-], ValidateData,optionController.updateOption)
+router.put('/:id', createUpdateRuleOption, ValidateData, optionController.updateOption)
 
 router.delete('/:id', optionController.deleteOption)
 
