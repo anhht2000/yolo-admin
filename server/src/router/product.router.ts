26471df3e9c6c120eb95bf19236d7a40645cbd46
tuananhcx2000 <tuananhcx2000@gmail.com>
2021-10-07 17:23:32 +0700

    create api for signin signup enduser

--- server/src/router/product.router.ts
+++ server/src/router/product.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import productController from '../controllers/product.controller';
+import checkToken from '../middleware/checktokenMiddleware';
 import { ValidateData } from '../middleware/commonMiddleware';
 import { upload } from '../middleware/multerMiddleware';
 import { createUpdateRuleProduct } from './../rules/productRouter.rule';
@@ -9,7 +10,7 @@ const router = Router();
 router.get('/', productController.getProduct);
 router.post('/add', upload.array('allImg'), createUpdateRuleProduct, productController.addProduct);
 router.put('/edit/:id', upload.array('allImg'), createUpdateRuleProduct, productController.updateProduct);
-router.delete('/delete/:id', productController.deleteProduct);
+router.delete('/delete/:id', checkToken, productController.deleteProduct);
 router.get('/search', productController.searchProduct);
 router.get('/sort', productController.sortProduct);
 router.get('/:productId', productController.getOneProduct);
